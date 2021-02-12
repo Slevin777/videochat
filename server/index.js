9da389cd707c67c07655b0b4ab420e61b6d0bf4f
slevin777 <vitaliy.galchinskiy@jointoit.com>
2021-02-12 16:11:37 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const routes = require('../routes');
-const cors = require('cors');
+// const cors = require('cors');
 
 const app = express();
-app.use(cors());
+// app.use(cors());
 app.use(express.static('public'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(req)
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+})
 //routes
 app.use('/', routes);
 
