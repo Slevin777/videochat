15309732d9a2c3497e273cb1b598e71eee4003b0
slevin777 <vitaliy.galchinskiy@jointoit.com>
2021-02-12 16:16:00 +0200

    cors

--- server/index.js
+++ server/index.js
@@ -14,14 +14,14 @@ app.use((req, res, next) => {
 
 
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  // res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 })
 //routes
